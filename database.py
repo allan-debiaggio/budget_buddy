0144eb574496b7dffc8f275722245b71d7e4868e
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-03-19 11:30:36 +0100

    database.py connector

--- database.py
+++ database.py
@@ -2,3 +2,16 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
+PASSWORD = os.getenv("PASSWORD")
+
+mydb = mysql.connector.connect(
+    user = "root",
+    password = PASSWORD,
+    host = "127.0.0.1",
+    database = "bank"
+)
+
+if mydb != None :
+    print("Connection... ESTABLISHED ! WOUUUUSHHHHHHH")

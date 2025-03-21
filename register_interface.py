64ece04f11d8f4b1edc11b4fd568ea574af53926
Matthieu-Odinot <matthieu.odinot@laplateforme.io>
2025-03-21 10:59:51 +0100

    Register interface

--- register_interface.py
+++ register_interface.py
@@ -9,7 +9,7 @@ ctk.set_default_color_theme("blue")
 root = ctk.CTk()
 root.title("Budget Buddy")
 root.geometry("1366x768")
-root.resizable(False, False)  # Lock window resizing
+root.resizable(False, False) 
 
 # Loading background image
 background_image = Image.open("asset/screen_connect.png")
@@ -19,43 +19,49 @@ background_photo = ctk.CTkImage(light_image=background_image, dark_image=backgro
 background_label = ctk.CTkLabel(root, image=background_photo, text="")
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# Adding title on background image
-title_label = ctk.CTkLabel(root, text="Budget Buddy", font=("Arial", 42), bg_color="transparent")
-title_label.place(relx=0.5, rely=0.1, anchor="center")
-
 # Creating frame to group elements
-frame = ctk.CTkFrame(root, width=360, height=414, fg_color="#4D4D4D", corner_radius=0)
+frame = ctk.CTkFrame(root, width=360, height=604, fg_color="#4D4D4D", corner_radius=0)
 frame.place(relx=0.5, rely=0.5, anchor="center")
 
 # Button functions definition
 def on_enter_click():
-    username = username_entry.get()  # Get text from username field
-    password = password_entry.get()  # Get text from password field
-    print(f"Username: {username}, Password: {password}")
+    first_name = first_name_entry.get() 
+    last_name = last_name_entry.get()
+    email = email_entry.get() 
+    password = password_entry.get() 
+
+    print(f"First name : {first_name}, Last name :{last_name},Email :{email}, Password: {password}")
     # Add login validation logic here
 
 def on_create_account_click():
     print("Create account button clicked!")
-    # Add account creation logic here
+
+
+def on_connect_click():
+    print("Connect button clicked!")
+
 
 # Adding UI elements to the frame
 
 # Connect label
-connect_label = ctk.CTkLabel(root, text="Connect", font=("Arial", 16), bg_color="#4D4D4D")
-connect_label.place(x=651, y=203)
+connect_label = ctk.CTkLabel(root, text="Register", font=("Arial", 16), bg_color="#4D4D4D")
+connect_label.place(x=651, y=103)
 
-# Username entry field
-username_entry = ctk.CTkEntry(
-    root,
-    width=313.04,
-    height=64,
-    placeholder_text="Username",
-    bg_color="#4D4D4D",
-    fg_color="#2C2C2C",
-    corner_radius=10,
-    font=("Arial", 14)
-)
-username_entry.place(x=527.43, y=238.4)
+def create_entry(placeholder_text, y_position):
+    return ctk.CTkEntry(
+        root,
+        width=313.04,
+        height=64,
+        placeholder_text=placeholder_text,
+        bg_color="#4D4D4D",
+        fg_color="#2C2C2C",
+        corner_radius=10,
+        font=("Arial", 14)
+    ).place(x=526, y=y_position)
+
+first_name_entry = create_entry("First Name", 148)
+last_name_entry = create_entry("Last Name", 242)
+email_entry = create_entry("Email", 336)
 
 # Password entry field
 password_entry = ctk.CTkEntry(
@@ -67,9 +73,9 @@ password_entry = ctk.CTkEntry(
     fg_color="#2C2C2C",
     corner_radius=10,
     font=("Arial", 14),
-    show="*"  # Hide entered text (for passwords)
+    show="*" 
 )
-password_entry.place(x=527.43, y=320)
+password_entry.place(x=526, y=429)
 
 # Enter button
 enter_button = ctk.CTkButton(
@@ -82,20 +88,19 @@ enter_button = ctk.CTkButton(
     fg_color="#2C2C2C",
     command=on_enter_click  # Link function to button
 )
-enter_button.place(x=741.81, y=473.6)
+enter_button.place(x=740, y=591)
 
 # Create account button
-create_account_button = ctk.CTkButton(
+connect_button = ctk.CTkButton(
     root,
     width=98.66,
     height=64,
-    text="Create an account",
+    text="Connect",
     corner_radius=10,
     bg_color="#4D4D4D",
     fg_color="#2C2C2C",
-    command=on_create_account_click
+    command=on_connect_click  # Link function to button
 )
-create_account_button.place(x=527, y=474)
+connect_button.place(x=526, y=591)
 
-# Launch main interface loop
 root.mainloop()
